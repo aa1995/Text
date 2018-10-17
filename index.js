1c22df7417bd8fd3b8bccdaf7d8bb34a82707fb6
aa1995 <42834351+aa1995@users.noreply.github.com>
2018-10-17 19:22:24 +0800

    飒飒

--- index.js
+++ index.js
@@ -1,171 +1,133 @@
-window.onload = function(){
-     var obox = document.getElementById('box');
-     var aer = document.getElementsByClassName('er');
-     var arr = [];
-     var olast = 1;
-     var zs = 0;
-     var timer;
-	 var timer1;
-	 var timer2;
-	 var timer3;
-	 var timer4;
-     var op = document.getElementById('p');
-     var ody = document.getElementById('dy');
-     var oindex = document.getElementsByClassName('index')[0];
-     var ap = document.getElementById('ap')
-     var oneclick = document.getElementsByClassName('yd')[0];
-     var obody = document.getElementsByTagName('body')[0]
-     console.log(obody.offsetWidth)
-     // if (obody.offsetWidth >= 768) {
-     // 	 alert('请换手机')
-     // 	 return;
-     // }
-	//首页测试开始
-	 function b(){
-	 	var oxh1 = document.getElementsByClassName('qc')[0];
-	    var oxh2 = document.getElementsByClassName('qc1')[0];
-	    var obox = document.getElementById('box');   
-	    var osp = document.getElementById('s');
-	    var num = 0;
-	    var sum = 0;
-	    var sun = 0;
-	    
 
-	    timer = setInterval(function(){
-	       oxh1.style.left = oxh1.offsetLeft + 20 + 'px';
-	       oxh2.style.left = oxh2.offsetLeft + 20 + 'px';
-	       if (oxh2.offsetLeft >= 0) {
-	       	   oxh1.style.left = 0;
-	       	   oxh2.style.left = -obox.offsetWidth+'px';
-	       }
-	    },15)
-	    op.style.opacity = 0
-	    timer1 = setInterval(function(){
-	    	num = num + 0.2
-	        op.style.opacity = num;
-	        if (num >= 1) {
-	        	num = 0;
-	        }
-	    },150) 
-	    timer4 = setInterval(function(){
-	        timer2 = setInterval(function(){
-		        sum = sum+5;
-		        osp.style['-webkit-transform'] = 'rotate(' + sum + 'deg)';
-		        if (sum >= 30) {
-		        	clearInterval(timer2);
-		            timer3 = setInterval(function(){
-		            	sum = sum - 5;
-		            	osp.style['-webkit-transform'] = 'rotate(' + sum + 'deg)';
-		            	if (sum <= 0) {
-		            		clearInterval(timer3);
-		            	}
-		            },20)
-		        }
-		    },20);
-	        sun = sun + 1;
-
-	        if (sun > 5) {
-	        	clearInterval(timer4);
-	        }
-	        // console.log(sun)
-	    },500)
-	 }
-	 //首页测试
-    
-     // 题目第二页开始
-
-     function  abc(sum){
-     	var timerq;
-     	var spend = 0;
-     	var spend1 = 0;
-     	var touming = 0;
-     	var hg = Number(obox.offsetHeight);
-     	var bb =  Number(aer[sum+1].offsetTop);
-     	var atm = document.getElementsByClassName('tm')[sum];
-        var adiv = atm.getElementsByTagName('div')
-        console.log(adiv)
-        for (var i = 0; i < 4; i++) {
-        	// console.log(adiv.length)
-	     	adiv[i].index = i;
-	     	adiv[i].onclick = function(){
-	     		clearInterval(timerq)    		
-	     		aer[sum+1].style.top = hg + 'px';
-	     		This = this;
-	     		timerq = setInterval(function(){
-	     			spend = spend + 20;
-                    touming = touming + 0.08;
-                    aer[sum].style.top = Number(aer[sum].offsetTop) -spend+ 'px';
-                    aer[sum].style.opacity = 1 - touming;
-                    aer[sum+1].style.top = Number(aer[sum+1].offsetTop) - spend + 'px';
-                    aer[sum+1].style.opacity = touming;
-                    if ( Number(aer[sum].offsetTop) <= -hg) {
-                    	clearInterval(timerq)
-                    	arr.push(This.index)
-                    	aer[sum+1].style.top = 0;
-                    	// console.log(arr)
-                    	if (arr.length >= 10) {
-                    	    zs = arr[0] + arr[1]  + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9] + 10;
-                    		if (zs <= 23) {
-                                 ap.innerHTML =  "<p>任性妈妈（废铁级）</p>很抱歉，你是个任性的妈妈，您的健康饮食意识薄弱。如果不加以改变，会对孩子身体健康造成意想不到的后果。为了您和你家人的健康与幸福，建议您对孩子及家庭的饮食和健康做出调整，密切关注饮食健康和相关咨询，尽力改善现在的饮食状况。"
-                    		}else if (zs > 23 && zs < 32) {
-                                 ap.innerHTML = "<p>合格妈妈（白银级）</p>您是个合格的妈妈，您对饮食健康知识的了解处于中等水平。在越来越注重饮食健康的今天，您没有落伍，但还需要努力，才能更好地保持并增进健康。你需要关注饮食健康方面的信息，提高健康意识，注重改变健康饮食方式和习惯"
-                    		}else if (zs >31 && zs <= 40) {
-                                 ap.innerHTML = "<p>完美妈妈（黄金级）</p>你是个完美的妈妈，很出色！能到达这个级数的人并不多，说明您有良好健康意识，非常了解如何健康地安排孩子的饮食，有高水准的饮食安全与营养方面的知识。"
-                    		}
-                    	}     
-                    }
-	     		},80)
-	       	}
-        }
-     }
-     abc(0)
-     abc(1)
-     abc(2)
-     abc(3)
-     abc(4)
-     abc(5)
-     abc(6)
-     abc(7)
-     abc(8)
-     abc(9) 
-     b() //首页函数
-
-     op.onclick = function(){
-        ody.style.display = 'block';
-        oindex.style.top = -20000 + 'px';
-        clearInterval(timer)
-        clearInterval(timer1)
-        clearInterval(timer2)
-        clearInterval(timer3)
-        clearInterval(timer4)
-     }
-    // 第二页结束
-   
-
-   // 第一页点击事件开始
-   oneclick.onclick = function(){
-     	clearInterval(timerq)
-   	    var timerq;
-     	var spend = 0;
-     	var spend1 = 0;
-     	var touming = 0;
-     	var hg = Number(obox.offsetHeight);
-     	oindex.style.top = hg + 'px';
-     	oindex.style.display = 'block'
-   	    timerq = setInterval(function(){
-		    spend = spend + 20;
-	        touming = touming + 0.06;
-	        oneclick.style.top = Number(oneclick.offsetTop) -spend+ 'px';
-	        oneclick.style.opacity = 1 - touming;
-	        oindex.style.top = Number(oindex.offsetTop) - spend + 'px';
-	        oindex.style.opacity = touming;
-	        if (oindex.offsetTop <= 0) { 
-	        	oindex.style.top = 0;
-	        	clearInterval(timerq)
-	        }
-	    },80)
-   }
-   // 第一页点击事件结束
-   // // 音乐控件
+	(function(doc,win){
+      var docEl = doc.documentElement,  //文档根标签
+        resizeEvent =  'orientationchange' in window ? 'orientationchang' :'resize'; //viewport变化事件源
+      var rescale = function(){
+        //重置方法
+        win.clientWidth = docEl.clientWidth;
+        if (!win.clientWidth) return;
+        // 改变DOM根节点fontSize大小的值;
+        // (屏幕宽度/设计图宽度) = 缩放或扩大的比例值;
+        docEl.style.fontSize = 40 * (win.clientWidth / 667) + 'px';
+      };
+      if (!doc.addEventListener) return;
+      win.addEventListener(resizeEvent, rescale, false);
+      doc.addEventListener('DOMContentLoaded', rescale, false);
+    })(document, window);  //适配
   
+
+  var oul = document.getElementsByTagName('ul')[0];
+  var Ali = oul.getElementsByTagName('li');
+  var Afont = oul.getElementsByClassName('font');
+  var Aimg = oul.getElementsByClassName('teleimg');
+
+
+  document.getElementById('app').style.width = document.documentElement.clientWidth + 'px';
+  document.getElementById('app').style.height = document.documentElement.clientHeight + 'px';
+  let Arrow = document.getElementsByClassName('xs')[0];
+  window.onload =function(){
+    let timer;
+    let num = 1;
+    let sum = 0;
+    timer = setInterval(function(){
+      num = num - 0.1;
+      sum = sum + 5;
+      if (num <= 0) {
+        Arrow.style.bottom = 0 + 'px';
+        Arrow.style.opacity = 1;
+        num = 1;
+        sum = 0;
+      }
+      Arrow.style.bottom = sum + 'px';
+      Arrow.style.opacity = num;
+    },100)
+  }
+
+
+function font(obj,index){
+  let timer;
+  let sum = 0;
+  timer = setInterval(function(){
+    sum = sum + 0.3;
+    obj[index].style.left = -14.174 + sum + 'rem';
+    if (obj[index].offsetLeft/22.489 >= 1.36) {
+      obj[index].style.left = 1.36 + 'rem';
+      clearInterval(timer)
+    }
+  },20)
 }
+
+
+  //向上滑动开始
+var startx, starty;
+//获得角度
+function getAngle(angx, angy) {
+    return Math.atan2(angy, angx) * 180 / Math.PI;
+};
+//根据起点终点返回方向 1向上 2向下 3向左 4向右 0未滑动
+function getDirection(startx, starty, endx, endy) {
+    var angx = endx - startx;
+    var angy = endy - starty;
+    var result = 0;
+
+    //如果滑动距离太短
+    if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+        return result;
+    }
+
+    var angle = getAngle(angx, angy);
+    if (angle >= -135 && angle <= -45) {
+        result = 1;
+    } else if (angle > 45 && angle < 135) {
+        result = 2;
+    } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+        result = 3;
+    } else if (angle >= -45 && angle <= 45) {
+        result = 4;
+    }
+    return result;
+  }
+
+
+
+
+var shu = -1;
+
+    //手指接触屏幕
+  document.addEventListener("touchstart", function(e){
+      if (shu > 6) {
+        return
+      }
+      startx = e.touches[0].pageX;
+      starty = e.touches[0].pageY;
+  }, false);
+  //手指离开屏幕
+
+
+  document.addEventListener("touchend", function(e) {
+    if (shu > 6 ) {
+      return
+    }
+    console.log(shu)
+    let endx, endy;
+    let timer;
+    let sum = 0;
+    shu++;
+    endx = e.changedTouches[0].pageX;
+    endy = e.changedTouches[0].pageY;
+    Ali[shu+1].style.top = document.documentElement.clientHeight + 'px';
+    let direction = getDirection(startx, starty, endx, endy);
+    if (direction == 1) {
+        timer = setInterval(function(){
+          sum = sum + 10;
+          Ali[shu].style.top = -sum + 'px';
+          Ali[shu+1].style.top = sum + 'px';
+          if (sum >= document.documentElement.clientHeight) {
+            Ali[shu+1].style.top = 0 + 'px';
+            clearInterval(timer);
+            font(Afont,shu)
+            font(Aimg,shu)
+          }
+        },20)
+    }
+  }, false);
